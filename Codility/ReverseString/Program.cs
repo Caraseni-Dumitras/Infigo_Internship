69aaf3b57965f8fe07861f79340cc4f54a989670
Dumitras <Caraseni.dumitras@gmai.com>
2023-04-14 09:34:54 +0300

    Add String Reverse Algorithms

---
+++ Codility/ReverseString/Program.cs
@@ -0,0 +1,29 @@
+﻿// Write a function that reverses a string. The input string is given as an array of characters s.
+//
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+//
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+//
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        char[] s = { 'h', 'e', 'l', 'l', 'o' };
+
+        ReverseString.ReverseStringMethod(s);
+        
+        foreach (var c in s)
+        {
+            Console.Write(c);
+        }
+        
+    }
+}
